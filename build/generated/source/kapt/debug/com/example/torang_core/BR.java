75add5e56dabe02a42a0aafae2b9c803bda4ca69
Sarang Yang <>
2022-04-29 15:48:42 +0900

    update

--- build/generated/source/kapt/debug/com/example/torang_core/BR.java
+++
@@ -1,9 +0,0 @@
-package com.example.torang_core;
-
-public class BR {
-  public static int _all = 0;
-
-  public static int filterViewModel = 1;
-
-  public static int vm = 2;
-}
--- build/generated/source/kapt/debug/com/sryang/screen_filter/BR.java
+++
@@ -1,9 +0,0 @@
-package com.sryang.screen_filter;
-
-public class BR {
-  public static int _all = 0;
-
-  public static int filterViewModel = 1;
-
-  public static int vm = 2;
-}
