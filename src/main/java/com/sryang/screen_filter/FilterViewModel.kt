07c791fb6c00f447b279737c7f080a86548f5886
Sarang Yang <>
2022-06-04 16:24:11 +0900

    update

--- src/main/java/com/sryang/screen_filter/FilterViewModel.kt
+++ src/main/java/com/sryang/screen_filter/FilterViewModel.kt
@@ -10,10 +10,12 @@ import com.example.torang_core.repository.FindRepository
 import com.example.torang_core.repository.MapRepository
 import com.example.torang_core.repository.NationRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@OptIn(InternalCoroutinesApi::class)
 @HiltViewModel
 class FilterViewModel @Inject constructor(
     private val mapRepository: MapRepository,
@@ -24,32 +26,21 @@ class FilterViewModel @Inject constructor(
 
     private val _uiState = MutableStateFlow(FilterUiState())
     val uiState: StateFlow<FilterUiState> = _uiState
-
-    val selected : StateFlow<NationItem> = nationRepository.getSelectNationItem()
-
-    /** 내 위치 */
-    var latitudeMyLocation: Double = 0.0
-    var longitudeMyLocation: Double = 0.0
-
-    /** 이 지역 위치 */
-    var northEastLatitude: Double = 0.0
-    var northEastLongitude: Double = 0.0
-    var southWestLatitude: Double = 0.0
-    var southWestLongitude: Double = 0.0
-
-    val showRestaurantCardAndFilter : StateFlow<Boolean> = findRepository.showRestaurantCardAndFilter()
-
+    val selected: StateFlow<NationItem> = nationRepository.getSelectNationItem()
+    val showRestaurantCardAndFilter: StateFlow<Boolean> =
+        findRepository.showRestaurantCardAndFilter()
     private val isShow = MutableLiveData<Boolean>()
+
 
     init {
         isShow.value = false
 
         viewModelScope.launch {
-            filterRepository.getCurrentFilter().collect { filter ->
+            filterRepository.getCurrentFilter().collect(FlowCollector { filter ->
                 _uiState.update {
                     it.copy(filter = filter)
                 }
-            }
+            })
         }
     }
 
@@ -86,7 +77,32 @@ class FilterViewModel @Inject constructor(
 
     fun searchBoundRestaurant() {
         viewModelScope.launch {
-            findRepository.searchBoundRestaurant()
+            searchBoundRestaurant(
+                mapRepository.getNorthEastLatitude(),
+                mapRepository.getNorthEastLongitude(),
+                mapRepository.getSouthWestLatitude(),
+                mapRepository.getSouthWestLongitude()
+            )
         }
+    }
+
+    private suspend fun searchBoundRestaurant(
+        northEastLatitude: Double,
+        northEastLongitude: Double,
+        southWestLatitude: Double,
+        southWestLongitude: Double
+    ) {
+        val filter = filterRepository.getFilter()
+        findRepository.searchRestaurant(
+            distances = filter.distances,
+            restaurantType = filter.restaurantTypes,
+            prices = filter.prices,
+            ratings = filter.ratings,
+            northEastLatitude = northEastLatitude,
+            northEastLongitude = northEastLongitude,
+            southWestLatitude = southWestLatitude,
+            southWestLongitude = southWestLongitude,
+            searchType = SearchType.BOUND
+        )
     }
 }
