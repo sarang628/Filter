40b6a6f290ec4e633e982b50c54f456262c35937
SRYANG <sarang628@naver.com>
2023-05-12 18:21:21 +0800

    update

--- library/build/tmp/kapt3/stubs/debug/com/sryang/screen_filter/ui/FilterKt.java
+++ library/build/tmp/kapt3/stubs/debug/com/sryang/screen_filter/ui/FilterKt.java
@@ -1,10 +1,20 @@
 package com.sryang.screen_filter.ui;
 
-@kotlin.Metadata(mv = {1, 8, 0}, k = 2, xi = 48, d1 = {"\u0000\b\n\u0000\n\u0002\u0010\u0002\n\u0000\u001a\b\u0010\u0000\u001a\u00020\u0001H\u0007\u00a8\u0006\u0002"}, d2 = {"Filter", "", "library_debug"})
+@kotlin.Metadata(mv = {1, 8, 0}, k = 2, xi = 48, d1 = {"\u0000\u0012\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u0002\u001a\b\u0010\u0000\u001a\u00020\u0001H\u0007\u001a\u0010\u0010\u0002\u001a\u00020\u00012\u0006\u0010\u0003\u001a\u00020\u0004H\u0007\u001a\b\u0010\u0005\u001a\u00020\u0001H\u0007\u00a8\u0006\u0006"}, d2 = {"Filter", "", "FilterButton", "text", "", "PreviewFilterButton", "library_debug"})
 public final class FilterKt {
     
     @androidx.compose.ui.tooling.preview.Preview
     @androidx.compose.runtime.Composable
     public static final void Filter() {
+    }
+    
+    @androidx.compose.runtime.Composable
+    public static final void FilterButton(@org.jetbrains.annotations.NotNull
+    java.lang.String text) {
+    }
+    
+    @androidx.compose.ui.tooling.preview.Preview
+    @androidx.compose.runtime.Composable
+    public static final void PreviewFilterButton() {
     }
 }
--- library/src/main/java/com/sryang/screen_filter/ui/Filter.kt
+++ library/src/main/java/com/sryang/screen_filter/ui/Filter.kt
@@ -1,26 +1,50 @@
 package com.sryang.screen_filter.ui
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.sryang.screen_filter.R
 
 @Preview
 @Composable
 fun Filter() {
-    Row {
-        Button(onClick = {}, Modifier.weight(1f).height(40.dp)) { Text(text = "음식종류") }
+    Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
+        FilterButton(text = "음식종류")
         Spacer(modifier = Modifier.width(3.dp))
-        Button(onClick = {}, Modifier.weight(1f).height(40.dp)) { Text(text = "가격") }
+        FilterButton(text = "가격")
         Spacer(modifier = Modifier.width(3.dp))
-        Button(onClick = {}, Modifier.weight(1f).height(40.dp)) { Text(text = "평점") }
+        FilterButton(text = "평점")
         Spacer(modifier = Modifier.width(3.dp))
-        Button(onClick = {}, Modifier.weight(1f).height(40.dp)) { Text(text = "거리") }
+        FilterButton(text = "거리")
     }
+}
+
+@Composable
+fun FilterButton(text: String) {
+    OutlinedButton(
+        onClick = {},
+        border = BorderStroke(1.dp, colorResource(id = R.color.colorPrimary)),
+    ) {
+        Text(text = text, color = colorResource(id = R.color.colorPrimary))
+    }
+}
+
+@Preview
+@Composable
+fun PreviewFilterButton() {
+    FilterButton(text = "ds")
 }
