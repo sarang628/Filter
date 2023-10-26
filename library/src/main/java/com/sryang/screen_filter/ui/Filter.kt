a13ecc20bff4c4e980ecb406b1048404daed6ed9
SRYANG <sarang628@naver.com>
2023-10-26 17:56:48 +0800

    update

--- library/src/main/java/com/sryang/screen_filter/ui/Filter.kt
+++ library/src/main/java/com/sryang/screen_filter/ui/Filter.kt
@@ -1,5 +1,8 @@
 package com.sryang.screen_filter.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.basicMarquee
@@ -10,7 +13,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
@@ -19,48 +21,58 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sryang.screen_filter.R
 
 @Composable
-fun Filter(filterViewModel: FilterViewModel, onFilter: (FilterUiState) -> Unit) {
+fun Filter(filterViewModel: FilterViewModel, onFilter: (FilterUiState) -> Unit, visible: Boolean) {
     val uiState: FilterUiState by filterViewModel.uiState.collectAsState()
-    Column(Modifier.padding(start = 8.dp, end = 8.dp)) {
-        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
-            FilterButton(text = "FoodType", onClick = {
-                filterViewModel.setType("FoodType")
-            })
-            Spacer(modifier = Modifier.width(3.dp))
-            FilterButton(text = "Price", onClick = {
-                filterViewModel.setType("Price")
-            })
-            Spacer(modifier = Modifier.width(3.dp))
-            FilterButton(text = "Rating", onClick = {
-                filterViewModel.setType("Rating")
-            })
-            Spacer(modifier = Modifier.width(3.dp))
-            FilterButton(text = "Distance", onClick = {
-                filterViewModel.setType("Distance")
-            })
-        }
-        if (uiState.type == "FoodType") FoodFilter(foodType = uiState.foodType,
-            onFoodType = { filterViewModel.setFoodType(it) })
-        if (uiState.type == "Price") PriceFilter(
-            price = uiState.price,
-            onPrice = { filterViewModel.setPrice(it) })
-        if (uiState.type == "Rating") RatingFilter(
-            rating = uiState.rating,
-            onRating = { filterViewModel.setRating(it) })
-        if (uiState.type == "Distance") DistanceFilter(distance = uiState.distance,
-            onDistance = { filterViewModel.setDistance(it) })
+    val density = LocalDensity.current
+    AnimatedVisibility(
+        visible = visible,
+        enter = slideInVertically { with(density) { -100.dp.roundToPx() } },
+        exit = slideOutVertically { with(density) { -100.dp.roundToPx() } }
+    ) {
+        Column(Modifier.padding(start = 8.dp, end = 8.dp)) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                FilterButton(text = "FoodType", onClick = {
+                    filterViewModel.setType("FoodType")
+                })
+                Spacer(modifier = Modifier.width(3.dp))
+                FilterButton(text = "Price", onClick = {
+                    filterViewModel.setType("Price")
+                })
+                Spacer(modifier = Modifier.width(3.dp))
+                FilterButton(text = "Rating", onClick = {
+                    filterViewModel.setType("Rating")
+                })
+                Spacer(modifier = Modifier.width(3.dp))
+                FilterButton(text = "Distance", onClick = {
+                    filterViewModel.setType("Distance")
+                })
+            }
+            if (uiState.type == "FoodType") FoodFilter(foodType = uiState.foodType,
+                onFoodType = { filterViewModel.setFoodType(it) })
+            if (uiState.type == "Price") PriceFilter(
+                price = uiState.price,
+                onPrice = { filterViewModel.setPrice(it) })
+            if (uiState.type == "Rating") RatingFilter(
+                rating = uiState.rating,
+                onRating = { filterViewModel.setRating(it) })
+            if (uiState.type == "Distance") DistanceFilter(distance = uiState.distance,
+                onDistance = { filterViewModel.setDistance(it) })
 
-        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
-            FilterButton(text = "필터적용", onClick = { onFilter.invoke(uiState) })
+            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                FilterButton(text = "필터적용", onClick = { onFilter.invoke(uiState) })
+            }
         }
     }
-
 }
 
 
