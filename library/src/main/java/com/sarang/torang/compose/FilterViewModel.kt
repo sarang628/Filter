51f2f0b72878d4024d8c11ebe992599168c2531a
sryang <sarang628@gmail.com>
2025-08-13 13:10:44 +0800

    move package
    - ui to compose

--- library/src/main/java/com/sarang/torang/compose/FilterViewModel.kt
+++ library/src/main/java/com/sarang/torang/compose/FilterViewModel.kt
@@ -1,6 +1,8 @@
 package com.sarang.torang.compose
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sarang.torang.data.City
