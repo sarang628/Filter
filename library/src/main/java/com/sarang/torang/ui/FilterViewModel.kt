2f840c882929557afdedc61136c75e1135f99461
sryang <sarang628@gmail.com>
2025-08-13 12:58:10 +0800

    arrange code

--- library/src/main/java/com/sarang/torang/ui/FilterViewModel.kt
+++ library/src/main/java/com/sarang/torang/ui/FilterViewModel.kt
@@ -29,8 +29,7 @@ class FilterViewModel @Inject constructor(
     val setFilterUseCase: SetFilterUseCase,
     val getFilterUseCase: GetFiltersUseCase
 ) : ViewModel() {
-    var uiState by mutableStateOf(FilterUiState())
-        private set
+    var uiState by mutableStateOf(FilterUiState()); private set
 
     init {
         viewModelScope.launch {
@@ -48,91 +47,29 @@ class FilterViewModel @Inject constructor(
             launch { getFilterUseCase.getDistance().collect { uiState = uiState.copy(distance = it) } }
             launch { getFilterUseCase.getFoodType().collect { uiState = uiState.copy(foodType = it) } }
         }
-
     }
 
-    fun setType(s: String) {
-        viewModelScope.launch {
-            uiState = uiState.copy(
-                type = if (uiState.type == s) "" else s
-            )
-        }
-    }
-
-    fun setFoodType(foodType: String) {
-        viewModelScope.launch {
-            setFilterUseCase.setFoodType(foodType)
-        }
-    }
-
-    fun setPrice(price: String) {
-        viewModelScope.launch {
-            setFilterUseCase.setPrice(price)
-        }
-    }
-
-    fun setRating(rating: String) {
-        viewModelScope.launch {
-            setFilterUseCase.setRating(rating)
-        }
-    }
-
-
-    fun setDistance(distance: String) {
-        viewModelScope.launch {
-            setFilterUseCase.setDistance(distance)
-        }
-    }
+    fun setQuery(keyword: String) { uiState = uiState.copy(keyword = keyword) }
+    fun onThisArea() { viewModelScope.launch { findThisAreaUseCase.invoke() } }
+    fun onFilter() { viewModelScope.launch { findFilterUseCase.invoke() } }
+    fun setType(s: String) { viewModelScope.launch { uiState = uiState.copy(type = if (uiState.type == s) "" else s) } }
+    fun setFoodType(foodType: String) { viewModelScope.launch { setFilterUseCase.setFoodType(foodType) } }
+    fun setPrice(price: String) { viewModelScope.launch { setFilterUseCase.setPrice(price) } }
+    fun setRating(rating: String) { viewModelScope.launch { setFilterUseCase.setRating(rating) } }
+    fun setDistance(distance: String) { viewModelScope.launch { setFilterUseCase.setDistance(distance) } }
 
     fun onCity(city: City? = null) {
         viewModelScope.launch {
-            if (city == null) {
-                uiState = uiState.copy(showCityFilter = !uiState.showCityFilter)
-
-            } else {
-                val nation = uiState.nations.find { it.id == city.nation }
-                uiState = uiState.copy(
-                    selectedNation = nation,
-                    selectedCity = city,
-                    filteredCities = if (nation == null) arrayListOf() else getCitiesByNationIdUseCase.invoke(
-                        nation.id
-                    ),
-                    showCityFilter = false
-                )
-            }
+            if (city == null) { uiState = uiState.copy(showCityFilter = !uiState.showCityFilter) }
+            else { val nation = uiState.nations.find { it.id == city.nation }
+                uiState = uiState.copy(selectedNation = nation, selectedCity = city, filteredCities = if (nation == null) arrayListOf() else getCitiesByNationIdUseCase.invoke(nation.id), showCityFilter = false) }
         }
     }
 
     fun onNation(nation: Nation? = null) {
         viewModelScope.launch {
-            if (nation == null) {
-                uiState = uiState.copy(showCityFilter = !uiState.showCityFilter)
-
-            } else {
-                viewModelScope.launch {
-                    uiState = uiState.copy(
-                        selectedNation = nation,
-                        selectedCity = null,
-                        filteredCities = getCitiesByNationIdUseCase.invoke(nation.id)
-                    )
-                }
-            }
-        }
-    }
-
-    fun setQuery(keyword: String) {
-        uiState = uiState.copy(keyword = keyword)
-    }
-
-    fun onThisArea() {
-        viewModelScope.launch {
-            findThisAreaUseCase.invoke()
-        }
-    }
-
-    fun onFilter() {
-        viewModelScope.launch {
-            findFilterUseCase.invoke()
+            if (nation == null) { uiState = uiState.copy(showCityFilter = !uiState.showCityFilter) }
+            else { viewModelScope.launch { uiState = uiState.copy(selectedNation = nation, selectedCity = null, filteredCities = getCitiesByNationIdUseCase.invoke(nation.id)) } }
         }
     }
 }
