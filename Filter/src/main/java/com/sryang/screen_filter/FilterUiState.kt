907c65a838721920e3f2fd01997d05cd271347db
sryang <sarang628@gmail.com>
2025-09-13 09:10:03 +0800

    add submodules

--- Filter/src/main/java/com/sryang/screen_filter/FilterUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.sryang.screen_filter
-
-import com.example.torang_core.data.model.Filter
-import com.example.torang_core.data.model.name
-
-data class FilterUiState(val filter: Filter = Filter())
-
-val FilterUiState.foodType: String get() = filter.restaurantTypes.name
